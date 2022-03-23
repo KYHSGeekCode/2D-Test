feca4f6224db01e19248d225b9ee2c7d433d281c
KYHSGeekCode <jourhyang123@gmail.com>
2022-03-24 01:16:50 +0900

    :tada: Begin

---
+++ Assets/Scripts/IngameScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ///gameObject.GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
